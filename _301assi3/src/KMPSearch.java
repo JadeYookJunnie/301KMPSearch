77ae4743a8854d842851545d323c2cd8978b09cd
sy1122young <90888243+sy1122young@users.noreply.github.com>
2023-05-18 17:37:49 +1200

    Add files via upload

---
+++ _301assi3/src/KMPSearch.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.io.File; // Import the File class
+import java.io.FileNotFoundException; // Import this class to handle errors
+import java.util.Scanner; // Import the Scanner class to read text files
+
+class KMPSearch {
+
+    public static void main(String[] args) {
+        String tableFile = "";
+        String textFile = "";
+        // get table file
+        tableFile = args[0];
+        readTable(tableFile);
+        // get text file
+        textFile = args[1];
+        // System.out.println(textFile);
+
+    }
+
+    public static void readTable(String filename) {
+        try {
+            File table = new File(filename);
+            Scanner myReader = new Scanner(table);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.print(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error spawned");
+            e.printStackTrace();
+        }
+    }
+
+    // SearchedText = the text we are searching for
+    public static void SearchText(String SearchedText) {
+
+    }
+}
