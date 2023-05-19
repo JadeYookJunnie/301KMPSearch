a7e5d2feb8340d44ece13427aab2def9986dff7c
JadeYookJunnie <jade.lemonade2@gmail.com>
2023-05-19 21:17:23 +1200

    reading skip array

--- _301assi3/src/KMPSearch.java
+++ _301assi3/src/KMPSearch.java
@@ -3,13 +3,14 @@ import java.util.List;
 import java.io.File; // Import the File class
 import java.io.FileNotFoundException; // Import this class to handle errors
 import java.util.Scanner; // Import the Scanner class to read text files
+import java.io.*;
 
 class KMPSearch {
 
-    public List<Character> charList = new ArrayList<>();
+    static List<Character> charList = new ArrayList<>();
 
-    public String passedString = ""; //target string
-    String[][] skipArray;
+    static String passedString = ""; //target string
+    static String[][] skipArray;
     public static void main(String[] args) {
         String tableFile = "";
         String textFile = "";
@@ -24,37 +25,64 @@ class KMPSearch {
     }
 
     public static void readTable(String filename) {
-        try {
-            File table = new File(filename);
-            //read first line as string to be compared
-            Scanner myReader = new Scanner(table);
-            //put characters into a row
-            //use index of character in searchstring
-            //if not, index = list.length
-            passedString = myReader.nextLine();
+//            //read first line as string to be compared
+//            //put characters into a row
+//            //use index of character in searchstring
+//            //if not, index = list.length
 
+        BufferedReader reader;
+        String pattern;
+
+        try {
+            List<String[]> rows = new ArrayList<String[]>();
+            reader = new BufferedReader(new FileReader(filename));
+            passedString = reader.readLine();
             for(int i = 0; i< passedString.length(); i++) {
                 if(!(charList.contains(passedString.charAt(i)))) {
-                    charList.add(subString.charAt(i));
+                    charList.add(passedString.charAt(i));
                 }
             }
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                //pass to array
-                System.out.print(data);
+            //System.out.println(passedString.length());
+            skipArray = new String[charList.size()+1][passedString.length()];
+
+            int x = 0;
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                // //break line into 2d array
+                String[] row = line.split(",");
+                // //row = String[]{0,1,0,3,2} x4
+                // rows.add(row); //add to list
+                // //pattern = reader.readLine();
+                // System.out.println(line);
+                // //break;
+
+                for (int i = 0; i < row.length; i++) {
+                    skipArray[x][i] = row[i];
+                    //System.out.println(row[i]);
+
+                    //System.out.print(skipArray[x][i]);
+                    //System.out.printf("%n");
+
+                }
+                x++;
+
             }
-            myReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An error spawned");
+            // read rest of file
+            //String[][] skipArray = new String[rows.size()][0];
+            //rows.toArray(skipArray);
+
+
+            reader.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     // SearchedText = the text we are searching for
     public static void SearchText(String textFile) {
-        //start = index position
+        // start = index position
         int start = 0;
-        //column
+        // column
         int offset = 0;
         int lineNumber = 0;
         // loop through text file
@@ -62,13 +90,20 @@ class KMPSearch {
             File text = new File(textFile);
             Scanner myReader = new Scanner(text);
             while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                if(data.contains(passedString)) {
+                String textline = myReader.nextLine();
+                if (textline.charAt(offset) == passedString.charAt(offset)) {
+                    // look at table and increment passedString
 
-
+                    //if we make it to end off passed string
+                    //output line
+                    //ouput index
+                } else {
+                    // look at table skip desired characters  on line
                 }
-                System.out.print(data);
+
+                System.out.print(textline);
                 lineNumber++;
+
             }
             myReader.close();
         } catch (FileNotFoundException e) {
